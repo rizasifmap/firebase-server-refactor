8752d42407c3de84b7d3e0153dd0503685c49eb9
rizwan asif <rizwan@mapproject.se>
2018-10-29 14:13:38 +0100

    #261 blocking users

--- Test Scripts/main.js
+++ Test Scripts/main.js
@@ -14,6 +14,10 @@ app.get('/search', function (req, res) {
   res.sendFile(path.join(__dirname+'/search.html'));
 })
 
+app.get('/block', function (req, res) {
+  res.sendFile(path.join(__dirname+'/block_user.html'));
+})
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
